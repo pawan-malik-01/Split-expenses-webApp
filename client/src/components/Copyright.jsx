1c05471771805619d2348e29b7344926bbf80592
Pawan Malik <124669097+pawan-malik-01@users.noreply.github.com>
2024-04-02 19:12:34 +0530

    Update Copyright.jsx

--- client/src/components/Copyright.jsx
+++ client/src/components/Copyright.jsx
@@ -8,7 +8,7 @@ export default function Copyright() {
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
             &copy; Pawan Malik | Open Source &nbsp; 
             {/* <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/> */}<br/>
-            <Link variant="subtitle3" component={'a'} href="https://github.com/pawan-malik-01" target="_blank">
+            <Link variant="subtitle3" component={'a'} href="https://github.com/pawan-malik-01/jdfhdf" target="_blank">
             [GitHub]
             </Link>
           </Typography>
